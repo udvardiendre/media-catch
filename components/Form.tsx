2679ddb208db3476719ddc9372b404f18952ba2a
eudvardi2 <udvardiendre@gmail.com>
2023-11-09 21:15:49 +0100

    Login, Sign-UP, Profile

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,5 +1,7 @@
 "use client"
+import Link from 'next/link'
 import React from 'react'
+
 
 type Props = {}
 
@@ -7,8 +9,11 @@ const Form = (props: Props) => {
   return (
     <div className='bg-secondary-bg max-w-[702px] min-[702px]:w-[702px] flex my-10 p-10 rounded shadow-md  w-full'>
         <form >
-            
+            <input type="text" />
         </form>
+
+        <button>Feltöltés</button>
+        <Link href="/profile">Vissza</Link>
     </div>
   )
 }
