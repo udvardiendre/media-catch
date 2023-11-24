aa41fd2e543ef8cd2d725341728b8ce93cbfa599
eudvardi2 <udvardiendre@gmail.com>
2023-11-24 16:23:41 +0100

    Initial

--- app/update-product/page.tsx
+++ app/update-product/page.tsx
@@ -4,7 +4,8 @@ import React from 'react'
 
 type Props = {}
 
-const UploadProduct = (props: Props) => {
+const UpdateProduct = (props: Props) => {
+    
   return (
     <section className="bg-primary-bg flex flex-col items-center justify-center ">
       <Form/>
@@ -12,4 +13,4 @@ const UploadProduct = (props: Props) => {
   )
 }
 
-export default UploadProduct
+export default UpdateProduct
