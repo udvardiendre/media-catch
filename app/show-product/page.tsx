ce0645f7d50b6113147e4f8c5615bf1d96f75df6
eudvardi2 <udvardiendre@gmail.com>
2023-12-08 00:32:55 +0100

    Search, Product Detail Feature

--- app/show-product/page.tsx
+++ app/show-product/page.tsx
@@ -4,19 +4,14 @@ import React from 'react'
 
 import {useState, useEffect} from 'react'
 import { useSearchParams } from "next/navigation"
+import Image from 'next/image'
 
 
 type Props = {};
 
 const ShowProduct = (props: Props) => {
 
-const [product, setProduct] = useState({
-    brand: String,
-    name: String,
-    price: String,
-    description: String,
-    photos: [String]
-})
+const [product, setProduct] = useState<{[key: string]: any}>({})
   const searchParams = useSearchParams();
   const productId = searchParams.get("id");
 
@@ -38,8 +33,21 @@ const [product, setProduct] = useState({
   }, [productId]);
   console.log(product)
   return (
-  <section>
-    <h1>Nacsá</h1>
+  <section className="bg-primary-bg flex flex-col items-center justify-center">
+    <div>
+      {product.photos ? (<Image width={620} height={406} src={product.photos[0] as string} alt="product_photo" />):(<p>Loading...</p>)}
+    </div>
+    <div>
+      <p>{`Ár: ${product.price} Ft`}</p>
+      <p>{product.brand}</p>
+      <p>{product.name}</p>
+      <p>Leírás</p>
+      <p>{product.description}</p>
+      <p>Feladás dátuma: .....</p>
+      <Link href="/cart">Kosárba teszem</Link>
+    </div>
+    <div></div>
+    <div></div>
   </section>);
 };
 
